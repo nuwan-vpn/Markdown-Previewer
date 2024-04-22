ca0df1e6d6bdf9728d817bd177b5577b85446875
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-22 16:27:10 +0530

    feat: Add Markdown preview functionality
    - Implement a textarea for Markdown input
    - Display live preview using ReactMarkdown
    - Set default Markdown text
    - Style textarea and preview sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ReactMarkdown from 'react-markdown';
 import './App.css'
 
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... **_both!_**
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+`;
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdownText, setMarkdownText] = useState<string>(defaultMarkdown)
 
   return (
     <>
+      <div>
+        <h1>Markdown Previewer</h1>
+        <div className="boxes-container">
+          <textarea name="editor" id="editor">
+            <div className="preview">
+              <ReactMarkdown>{markdownText}</ReactMarkdown>
+            </div>
+          </textarea>
+        </div>
+      </div>
       
     </>
   )
